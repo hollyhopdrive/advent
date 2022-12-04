fc4423bb9ae329fbabe6f933847d061e2b4be7c3
Paul McArthur <hollyhopdrive@proton.me>
2022-12-04 15:09:01 -0800

    day 4 refactor - remove redundancy

--- 2022/src/main/java/day4/solutions/Runner.java
+++ 2022/src/main/java/day4/solutions/Runner.java
@@ -2,21 +2,23 @@ package day4.solutions;
 
 import java.util.List;
 
+import day4.solutions.overlaps.Analyzer;
 import day4.solutions.overlaps.Overlap;
 import shared.Counter;
 import shared.InputReader;
 
 public class Runner {
 
-    public static int run(final String path, final Overlap overlap) {
+    public static int run(final String path, final Overlap overlapType) {
         final List<String> input = new InputReader().readInputFile(path);
+        final Analyzer overlapAnalyzer = new Analyzer(overlapType);
         final Counter overlaps = new Counter();
-        input.forEach(l -> { if(processPair(l, overlap)) { overlaps.increment(); } });
+        input.forEach(l -> { if(processPair(l, overlapAnalyzer)) { overlaps.increment(); } });
         return overlaps.getValue();
     }
 
-    private static boolean processPair(final String pair, final Overlap overlap) {
+    private static boolean processPair(final String pair, final Analyzer overlapAnalyzer) {
         String[] assignments = pair.split(",");
-        return overlap.overlaps(assignments[0], assignments[1]);
+        return overlapAnalyzer.overlaps(assignments[0], assignments[1]);
     }
 }
--- 2022/src/main/java/day4/solutions/overlaps/Partial.java
+++ 2022/src/main/java/day4/solutions/overlaps/Partial.java
@@ -1,15 +1,9 @@
 package day4.solutions.overlaps;
 
 public class Partial implements Overlap {
-
+    
     @Override
-    public boolean overlaps(final String lhs, final String rhs) {
-        final Integer[] lhsRange = splitRange(lhs);
-        final Integer[] rhsRange = splitRange(rhs);
-        return rangeOverlaps(lhsRange, rhsRange) || rangeOverlaps(rhsRange, lhsRange); 
-    }
-
-    private boolean rangeOverlaps(Integer[] lhs, Integer[] rhs) {
+    public boolean overlaps(Integer[] lhs, Integer[] rhs) {
         return lhs[0] >= rhs[0] && lhs[0] <= rhs[1]; 
     }
 }
