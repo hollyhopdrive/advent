cb777df5f7c45bbaccb50fca8fd5d1bcc6ac0cb7
Paul McArthur <hollyhopdrive@proton.me>
2022-12-05 20:54:47 -0800

    prep day 7

---
+++ 2022/src/main/java/day7/solutions/Runner.java
@@ -0,0 +1,16 @@
+package day7.solutions;
+
+import java.util.List;
+
+import shared.Counter;
+import shared.InputReader;
+
+public class Runner {
+
+    public static int run(final String path) {
+        final List<String> input = new InputReader().readInputFile(path);
+        final Counter answer = new Counter();
+        input.forEach(l -> answer.increment());
+        return answer.getValue();
+    }
+}
---
+++ 2022/src/test/java/day7/solutions/SolutionTest.java
@@ -0,0 +1,28 @@
+package day7.solutions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void testPuzzle1Sample() {
+        assertEquals(0, Runner.run("day6/sample.txt"));
+    }
+    
+    @Test
+    public void testPuzzle1Real() {
+        assertEquals(0, Runner.run("day6/input.txt"));
+    }
+    
+    @Test
+    public void testPuzzle2Sample() {
+        assertEquals(0, Runner.run("day6/sample.txt"));
+    }
+    
+    @Test
+    public void testPuzzle2Real() {
+        assertEquals(0, Runner.run("day6/input.txt"));
+    }
+}
